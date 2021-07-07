e7f272bc420911801bb7aee2428c6feff741dd29
Felipe Rodrigues <contactfeliper@gmail.com>
2021-07-06 22:47:34 -0300

    Inserindo Jakarta Persistence ao projeto!

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -4,27 +4,19 @@ import com.algaworks.algalog.domain.model.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @RestController
 public class ClienteController {
 
+    @PersistenceContext // Injeta o entity manager na variável.
+    private EntityManager manager;
+
     @GetMapping("/clientes")
     public List<Cliente> listar(){
-
-        Cliente cliente1 = new Cliente();
-        cliente1.setId(1L);
-        cliente1.setNome("Felipe Rodrigues");
-        cliente1.setEmail("2000feliperodrigues@gmail.com");
-        cliente1.setTelefone("+55 (42) 98442-4077");
-
-        Cliente cliente2 = new Cliente();
-        cliente2.setId(2L);
-        cliente2.setNome("Gilmar Soares");
-        cliente2.setEmail("gilmar@gmail.com");
-        cliente2.setTelefone("+55 (42) 92212-7840");
-
-        return Arrays.asList(cliente1, cliente2);
+        return manager.createQuery("from Cliente", Cliente.class)
+                .getResultList();
     }
 }
--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -1,15 +1,28 @@
 package com.algaworks.algalog.domain.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true) // Gera os equals e os hash para os atributos escolhidos.
 @Getter
 @Setter
+@Entity // Define que a classe pertence a uma entidade.
+        // O mesmo já busca o nome da classe como o nome da tabela no banco.
+// Caso o nome da tabela seja diferente do nome da classe se usa o @Table(name = "nomeTabela").
 public class Cliente {
 
+    @EqualsAndHashCode.Exclude // Seleciona para gerar os equals e hash code.
+    @Id // Identifica a chave primária.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Define a geração do Id para o padrão do banco(deixa como auto incremento)
     private Long id;
+
     private String nome;
     private String email;
+
+    @Column(name = "fone") // Representa o nome da coluna no banco. Caso seja o mesmo não precisa da anotação.
     private String telefone;
 
 }
