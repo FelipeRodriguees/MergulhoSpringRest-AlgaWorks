954f37b3f5750aa799d69690aed5c5a306d0685d
Felipe Rodrigues <contactfeliper@gmail.com>
2021-07-08 22:21:50 -0300

    Usando Spring DataJPA

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,22 +1,23 @@
 package com.algaworks.algalog.api.controller;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @RestController
 public class ClienteController {
 
-    @PersistenceContext // Injeta o entity manager na variável.
-    private EntityManager manager;
+    private ClienteRepository clienteRepository;
+
+    public ClienteController(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
 
     @GetMapping("/clientes")
     public List<Cliente> listar(){
-        return manager.createQuery("from Cliente", Cliente.class)
-                .getResultList();
+        return clienteRepository.findClienteByNomeContaining("u");
     }
 }
