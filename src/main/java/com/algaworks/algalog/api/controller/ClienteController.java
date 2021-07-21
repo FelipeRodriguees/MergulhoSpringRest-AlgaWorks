2553025e2d7470d81a822716c151acc4ce6db9d9
Felipe Rodrigues <contactfeliper@gmail.com>
2021-07-20 22:34:07 -0300

    Buscando um cliente pelo id

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -2,12 +2,17 @@ package com.algaworks.algalog.api.controller;
 
 import com.algaworks.algalog.domain.model.Cliente;
 import com.algaworks.algalog.domain.repository.ClienteRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
     private ClienteRepository clienteRepository;
@@ -16,8 +21,24 @@ public class ClienteController {
         this.clienteRepository = clienteRepository;
     }
 
-    @GetMapping("/clientes")
+    @GetMapping
     public List<Cliente> listar(){
         return clienteRepository.findClienteByNomeContaining("u");
+    }
+
+    @GetMapping("/{clienteId}")
+    public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+        return clienteRepository.findById(clienteId)
+                .map(cliente -> ResponseEntity.ok(cliente))
+                .orElse(ResponseEntity.notFound().build());
+
+
+// Ambos os códigos fazem a mesma coisa
+//        Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+//        if (cliente.isPresent()) {
+//            return ResponseEntity.ok(cliente.get());
+//        } else {
+//            return ResponseEntity.notFound().build();
+//        }
     }
 }
